7dfa74ce6f0ac3480f12e4cfb74a28043e1d1fa2
Pooja Reddy Kadari <98347320+pooja0207k@users.noreply.github.com>
2022-03-07 06:58:56 +0530

    Update netsuiterealtimeexport.js

--- NETSUITE REALTIME - REST/netsuiterealtimeexport.js
+++ NETSUITE REALTIME - REST/netsuiterealtimeexport.js
@@ -3,7 +3,7 @@ request({url:"https://api.staging.integrator.io/v1/exports",
 method:"POST",
 json:true,
 headers:{
-    "Authorization":"Bearer bf626c00d1b842598fd5cb611de13a9a"
+    "Authorization":"Bearer {token}"
 },
 body:{
     _id: "62252281e1bce1128d191779",
