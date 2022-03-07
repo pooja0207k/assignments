9f9710f4d939110907d16c6047ce6ec3177369ef
Pooja Reddy Kadari <98347320+pooja0207k@users.noreply.github.com>
2022-03-07 07:00:49 +0530

    Update netsuiteexportcustomer.js

--- Netsuite-ftp via program/netsuiteexportcustomer.js
+++ Netsuite-ftp via program/netsuiteexportcustomer.js
@@ -3,7 +3,7 @@ request({url:"https://api.staging.integrator.io/v1/exports",
 method:"POST",
 json:true,
 headers:{
-    "Authorization":"Bearer bf626c00d1b842598fd5cb611de13a9a"
+    "Authorization":"Bearer {token}"
 },
 body:{
     _id: "622542fec9d71b19a63b00e0",
